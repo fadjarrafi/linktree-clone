d43138b5f2ecc7d78e823fbc20b7e15f0d50ba10
Fadjar Irfan rafi <dark.fir21@gmail.com>
2023-05-26 21:31:43 +0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -53,26 +53,40 @@ function GitHubIcon() {
   );
 }
 
-function LinkCard({href, title, image}: {href: string; title: string; image?: string}) {
+function LinkCard({
+  href,
+  title,
+  image,
+}: {
+  href: string;
+  title: string;
+  image?: string;
+}) {
   return (
-    <a href={href} target="_blank" rel="noopener noreferrer" className='flex items-center p-1 w-full rounded-md hover:scale-105 transition-all bg-gray-100 mb-3 max-w-3xl'>
-
+    <a
+      href={href}
+      target="_blank"
+      rel="noopener noreferrer"
+      className="flex items-center p-1 w-full rounded-md hover:scale-105 transition-all bg-gray-100 mb-3 max-w-3xl"
+    >
       <div className="flex text-center w-full">
-        <div className="w-10">
+        <div className="w-10 h-10">
           {image && (
-          <Image
-              className='rounded-sm'
+            <Image
+              className="rounded-sm"
               alt={title}
               src={image}
               width={40}
               height={40}
-          />
-        )}
+            />
+          )}
         </div>
-        <h2 className="flex justify-center items-center font-semibold w-full text-gray-700 -ml-10">{title}</h2>
+        <h2 className="flex justify-center items-center font-semibold w-full text-gray-700 -ml-10">
+          {title}
+        </h2>
       </div>
     </a>
-  )
+  );
 }
 
 interface Data {
@@ -97,10 +111,11 @@ export default async function HomePage() {
   const data: Data | undefined = await get('linktree');
 
   if (!data) {
+    // not working yet https://github.com/vercel/next.js/issues/44232
     redirect('https://youtu.be/BBJa32lCaaY');
   }
 
-   return (
+  return (
     <div className="flex items-center flex-col mx-auto w-full justify-center mt-16 px-8">
       <Image
         priority
