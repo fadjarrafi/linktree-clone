8d2b91c3d8b0ade5deb2bd9e2544ec3af5572ca1
Fadjar Irfan rafi <dark.fir21@gmail.com>
2023-12-09 23:12:22 +0700

    Update link and Image Profile

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['pbs.twimg.com', "github.com"],
+        domains: ['pbs.twimg.com', "github.com", "square-indonesia.vercel.app", "www.freepnglogos.com"],
     },
 }
 
